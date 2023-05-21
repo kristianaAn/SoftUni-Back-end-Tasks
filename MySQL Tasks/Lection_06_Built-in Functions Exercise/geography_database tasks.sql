90b9fb853609ed6d006dbd7ffce5c3db369fa772
kristianaAn <christie.chris888@gmail.com>
2023-05-22 00:34:35 +0300

    Added new folder

---
+++ MySQL Tasks/Lection_06_Built-in Functions Exercise/geography_database tasks.sql
@@ -0,0 +1,23 @@
+-- Part II – Queries for Geography Database
+-- 10. Countries Holding 'A' 3 or More Times
+SELECT 
+    `country_name`, `iso_code`
+FROM
+    `countries`
+WHERE
+    `country_name` LIKE '%a%a%a%'
+ORDER BY `iso_code`;
+
+-- 11. Mix of Peak and River Names
+SELECT 
+    `peak_name`,
+    `river_name`,
+    LOWER(CONCAT(`peak_name`,
+                    SUBSTRING(`river_name`,
+                        2))) AS `mix`
+FROM
+    `peaks`,
+    `rivers`
+WHERE
+    RIGHT(`peak_name`, 1) = LEFT(`river_name`, 1)
+ORDER BY `mix` ASC;
