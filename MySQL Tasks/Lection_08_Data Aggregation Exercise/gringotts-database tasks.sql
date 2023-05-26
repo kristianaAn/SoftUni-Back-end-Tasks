bce45cf7fae76dcf11c83cfc18f4a8b80135a4a3
kristianaAn <christie.chris888@gmail.com>
2023-05-26 19:38:02 +0300

    Added new tasks

---
+++ MySQL Tasks/Lection_08_Data Aggregation Exercise/gringotts-database tasks.sql
@@ -0,0 +1,41 @@
+USE `gringotts`;
+
+-- 1. Records' Count
+SELECT 
+    COUNT(*) AS `count`
+FROM
+    `wizzard_deposits`;
+    
+-- 2. Longest Magic Wand
+SELECT 
+    MAX(`magic_wand_size`) AS `longest_magic_wand`
+FROM
+    `wizzard_deposits`;
+    
+-- 3. Longest Magic Wand Per Deposit Groups
+SELECT 
+    `deposit_group`,
+    MAX(`magic_wand_size`) AS `longest_magic_wand`
+FROM
+    `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `longest_magic_wand` ASC , `deposit_group` ASC;
+
+-- 4. Smallest Deposit Group Per Magic Wand Size*
+SELECT 
+    `deposit_group`
+FROM
+    `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY AVG(`deposit_group`) ASC
+LIMIT 1;
+
+-- 5. Deposits Sum
+SELECT 
+    `deposit_group`, SUM(`deposit_amount`) AS `total_sum`
+FROM
+    `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `total_sum` ASC;
+
+
