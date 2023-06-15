3e9e7739137e4c33bfe97e0856f4fa470421468d
kristianaAn <106185706+kristianaAn@users.noreply.github.com>
2023-06-15 11:26:36 +0300

    Update Main.java

--- Spring Data/DB Apps Introduction/SimpleDataRetrievalDemo/src/com/company/Main.java
+++ Spring Data/DB Apps Introduction/SimpleDataRetrievalDemo/src/com/company/Main.java
@@ -7,18 +7,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws SQLException {
         Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter username default (root): ");
-//        String user = sc.nextLine();
-//        user = user.equals("") ? "root" : user;
-//        System.out.println();
-//
-//        System.out.print("Enter password default (empty):");
-//        String password = sc.nextLine().trim();
-//        System.out.println();
+       System.out.print("Enter username default (root): ");
+       String user = sc.nextLine();
+       user = user.equals("") ? "root" : user;
+       System.out.println();
+
+       System.out.print("Enter password default (empty):");
+       String password = sc.nextLine().trim();
+       System.out.println();
 
         Properties props = new Properties();
-        props.setProperty("user", "root");
-        props.setProperty("password", "~123456");
+        props.setProperty("user", user);
+        props.setProperty("password", password);
 
         Connection connection = DriverManager
                 .getConnection("jdbc:mysql://localhost:3306/soft_uni", props);
