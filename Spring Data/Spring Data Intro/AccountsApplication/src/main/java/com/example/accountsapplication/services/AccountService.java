fee1d3736324c82421ec03bcde5e803912a3bc60
kristianaAn <christie.chris888@gmail.com>
2023-07-19 22:08:29 +0300

    Added new tasks

--- Spring Data/Spring Data Intro/AccountsApplication/src/main/java/com/example/accountsapplication/services/AccountService.java
+++
@@ -1,8 +0,0 @@
-package com.example.accountsapplication.services;
-
-import java.math.BigDecimal;
-
-public interface AccountService {
-    void withdrawMoney(BigDecimal money, Long id);
-    void transferMoney(BigDecimal money, Long id);
-}
--- Spring Data/Spring Data Intro/AccountsApplication/src/main/java/com/example/accountsapplication/services/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.accountsapplication.services;
-
-import com.example.accountsapplication.models.User;
-
-public interface UserService {
-    void registerUser(User user);
-}
